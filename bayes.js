f13f4e28820cebf78bb81a6db58464dcf0ae29d3
dboldure <dboldure@ucm.es>
2021-05-25 19:11:00 +0200

    lloyd

--- bayes.js
+++ bayes.js
@@ -3,10 +3,10 @@ const NAttr = 4;
 var bayesClassesFile = null;
 var bayesClassesInfo = [];
 
-function init(){
+function init() {
     console.log('Medias');
     const medias = [];
-    for(var i=0; i < bayesClassesInfo.length; i++){
+    for (var i = 0; i < bayesClassesInfo.length; i++) {
         medias.push(calculateMedia(bayesClassesInfo[i].data));
     }
     console.log(medias);
@@ -14,14 +14,14 @@ function init(){
 
     console.log('Covariances');
     const covariances = [];
-    for(var i=0; i < bayesClassesInfo.length; i++){
-        covariances.push(calculateCovariance(bayesClassesInfo[i].data,medias[i]));
+    for (var i = 0; i < bayesClassesInfo.length; i++) {
+        covariances.push(calculateCovariance(bayesClassesInfo[i].data, medias[i]));
     }
     console.log(covariances);
 
     console.log('Distances');
     // const distances = calculateDistance([[5.1],[3.5],[1.4],[0.2]], covariances, medias);
-    const distances = calculateDistance([[6.9],[3.1],[4.9],[1.5]], covariances, medias);
+    const distances = calculateDistance([[6.9], [3.1], [4.9], [1.5]], covariances, medias);
     // const distances = calculateDistance([[5.0],[3.4],[1.5],[0.2]], covariances, medias);
     console.log(distances);
 
@@ -33,66 +33,66 @@ function init(){
 
 // m = 1/n * Sum(1,n) xi
 // m = 1/bClass.length * Sum(1,NAttr) xi
-function calculateMedia(bClass){
+function calculateMedia(bClass) {
     let m = [];
 
-    for(var i=0; i < NAttr; i++){
+    for (var i = 0; i < NAttr; i++) {
         let sum = 0;
-        for(var j = 0; j < bClass.length; j++){
+        for (var j = 0; j < bClass.length; j++) {
             sum += parseFloat(bClass[j][i][0]);
         }
         m.push([sum]);
     }
 
-    for(var i=0; i < m.length; i++){
-        m[i][0] = (1/bClass.length) * m[i][0];
+    for (var i = 0; i < m.length; i++) {
+        m[i][0] = (1 / bClass.length) * m[i][0];
     }
 
     return m;
 }
 
 // C = 1/n * Sum(1,n)(xi - m)*(xi - m)t
-function calculateCovariance(bClass, media){
+function calculateCovariance(bClass, media) {
     let C = [];
     const XM = [];
 
-    for(var i=0; i < bClass.length; i++){
-        const xSubM =[];
-        for(var j = 0; j <NAttr; j++){
+    for (var i = 0; i < bClass.length; i++) {
+        const xSubM = [];
+        for (var j = 0; j < NAttr; j++) {
             xSubM.push(bClass[i][j][0] - media[j][0]);
         }
         XM.push([xSubM]);
     }
 
-    for(var i=0; i < XM.length; i++){
-        C.push(math.multiply(math.transpose(XM[i]),XM[i]));
+    for (var i = 0; i < XM.length; i++) {
+        C.push(math.multiply(math.transpose(XM[i]), XM[i]));
     }
 
     let sum = C[0];
-    for(var i=1; i < C.length; i++){
-        sum = math.add(sum,C[i]);
+    for (var i = 1; i < C.length; i++) {
+        sum = math.add(sum, C[i]);
     }
 
-    C = math.multiply(sum, 1/bClass.length);
+    C = math.multiply(sum, 1 / bClass.length);
 
     return C;
 }
 
 //C^-1 = Identity
-function calculateDistance(x,covariances, medias){
+function calculateDistance(x, covariances, medias) {
     distances = [];
     const xSubMList = [];
 
-    for(var i=0; i < medias.length; i++){
-        xSubMList.push(math.subtract(x,medias[i]))
+    for (var i = 0; i < medias.length; i++) {
+        xSubMList.push(math.subtract(x, medias[i]))
     }
 
 
-    for(var i=0; i < xSubMList.length; i++){
+    for (var i = 0; i < xSubMList.length; i++) {
         // const matrixResult = math.multiply(xSubMList[i],math.transpose(xSubMList[i]));
-        const matrixResult = math.multiply(xSubMList[i],math.transpose(xSubMList[i]), math.inv(covariances[i]));
+        const matrixResult = math.multiply(xSubMList[i], math.transpose(xSubMList[i]), math.inv(covariances[i]));
         let sum = 0;
-        for(var j=0; j < matrixResult.length; j++){
+        for (var j = 0; j < matrixResult.length; j++) {
             sum += matrixResult[j][j];
         }
         distances.push(sum);
@@ -101,40 +101,40 @@ function calculateDistance(x,covariances, medias){
     return distances;
 }
 
-function getMinorD(distances){
+function getMinorD(distances) {
     let minor = distances[0];
     let idx = 0;
 
-    for(var i=0; i < distances.length; i++){
-        if(distances[i] < minor){
+    for (var i = 0; i < distances.length; i++) {
+        if (distances[i] < minor) {
             minor = distances[i];
             idx = i;
         }
     }
 
-    return {n: minor, c: bayesClassesInfo[idx].classBName};
+    return { n: minor, c: bayesClassesInfo[idx].classBName };
 }
 
-function processFile(){
+function processFile() {
     classesInfo = [];
     const fileContent = bayesClassesFile.split('\n');
 
-    for(var i=0; i < fileContent.length; i++){
-        splitContent= fileContent[i].split(',');
-        for(var j=0; j < splitContent.length; j++){
+    for (var i = 0; i < fileContent.length; i++) {
+        splitContent = fileContent[i].split(',');
+        for (var j = 0; j < splitContent.length; j++) {
             splitContent[j] = splitContent[j].split('\r')[0];
         }
-        const className = splitContent[splitContent.length-1];
+        const className = splitContent[splitContent.length - 1];
 
-        splitContent = splitContent.splice(0,splitContent.length-1);
-        for(var z = 0; z < splitContent.length; z++){
+        splitContent = splitContent.splice(0, splitContent.length - 1);
+        for (var z = 0; z < splitContent.length; z++) {
             splitContent[z] = [splitContent[z]];
         }
 
-        const idx = bayesClassesInfo.findIndex((elem)=>{return elem.classBName === className});
-        if(idx === -1){
-            bayesClassesInfo.push({classBName: className, data: [splitContent]})
-        }else{
+        const idx = bayesClassesInfo.findIndex((elem) => { return elem.classBName === className });
+        if (idx === -1) {
+            bayesClassesInfo.push({ classBName: className, data: [splitContent] })
+        } else {
             bayesClassesInfo[idx].data.push(splitContent);
         }
     }
@@ -163,6 +163,6 @@ function readBayesFile() {
 }
 
 
-window.onload = function(){
-    document.getElementById('read-data-button').addEventListener('click',readBayesFile);
+window.onload = function () {
+    document.getElementById('read-data-button').addEventListener('click', readBayesFile);
 };
---
+++ lloyd.js
@@ -0,0 +1,98 @@
+"use strict";
+
+let epsilon = 0.0000000001;
+let centroidMatrix = [[], [4.6, 3.0, 4.0, 0.0], [6.8, 3.4, 4.6, 0.7]];
+let OldCentroidMatrix = [[], [4.6, 3.0, 4.0, 0.0], [6.8, 3.4, 4.6, 0.7]];
+
+function refreshClass(v, sample) {
+    let sol = 0;
+    let sampleMatrix = math.matrix([[Number(sample[0])], [Number(sample[1])], [Number(sample[2])], [Number(sample[3])]]);
+    let c = math.matrix([[v[0]], [v[1]], [v[2]], [v[3]]]);
+    let subtractAndkMultiply = math.multiply(math.subtract(sampleMatrix, c), 0.1);
+    sol = math.add(c, subtractAndkMultiply);
+    return sol;
+}
+
+function updateCentroids(data) {
+
+    for (let j = 0; j < data.length; j++) {
+        const sample = data[j].split(",");
+
+        if (calculateD(1, sample) < calculateD(2, sample)) { // se actualiza c1
+            let sol = refreshClass(centroidMatrix[1], sample);
+            centroidMatrix[1] = [sol.get([0, 0]), sol.get([1, 0]), sol.get([2, 0]), sol.get([3, 0])];
+        }
+        else {// se actualiza c2
+            let sol2 = refreshClass(centroidMatrix[2], sample);
+            centroidMatrix[2] = [sol2.get([0, 0]), sol2.get([1, 0]), sol2.get([2, 0]), sol2.get([3, 0])];
+        }
+    }
+};
+
+function calculateD(i, dato) {
+    let dij = 0;
+    let centroid = centroidMatrix[i];
+
+    for (let j = 0; j < dato.length - 1; j++) {
+        let base = dato[j] - centroid[j];
+        dij += Math.pow(base, 2)
+    }
+
+    return Math.sqrt(dij);
+};
+
+function lloyd() {
+    const fileContent = bayesClassesFile.split('\n');
+    let valueS = false;
+
+    for (let i = 1; i < 10 && !valueS; i++) {
+        updateCentroids(fileContent);
+
+        if (math.subtract(centroidMatrix[1], OldCentroidMatrix[1]) < epsilon && math.subtract(centroidMatrix[2], OldCentroidMatrix[2]) < epsilon)
+            valueS = true;
+
+        OldCentroidMatrix = centroidMatrix.slice();
+    }
+
+}
+
+function readlloydFile() {
+    if (document.querySelector("#dataFile-input2").files.length == 0) {
+        alert('Error : No file selected');
+        return;
+    }
+
+    let file = document.querySelector("#dataFile-input2").files[0];
+    let reader = new FileReader();
+
+    reader.addEventListener('load', function (e) {
+        bayesClassesFile = e.target.result;
+        lloyd();
+        let v1 = centroidMatrix[1];
+        let v2 = centroidMatrix[2];
+        console.log(v1);
+        console.log(v2)
+    });
+
+    reader.addEventListener('error', function () {
+        alert('Error : Failed to read attributes file');
+    });
+
+    reader.readAsText(file);
+}
+
+function solveLloyd() {
+    let sample = document.getElementById("ejemplo").value.split(",");
+
+    let resultado1 = (1 / calculateD(1, sample)) / ((1 / calculateD(1, sample)) + (1 / calculateD(2, sample)));
+    let resultado1 = (1 / calculateD(2, sample)) / ((1 / calculateD(1, sample)) + (1 / calculateD(2, sample)));
+
+    if (resultado1 > resultado2)
+      console.log('Iris-setosa');
+    else
+        console.log('Iris-versicolor');
+};
+
+window.onload = function () {
+    document.getElementById('read-data-button2').addEventListener('click', readlloydFile);
+};
